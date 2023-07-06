1ba462311a6610bd5a2b892e3b56240ba978407b
Bhargav <95201460+bhargavesh-dakka@users.noreply.github.com>
2023-07-06 15:25:45 +0530

    final

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import streamlit as st
 from streamlit_extras import switch_page_button
+import mysql.connector as mysql
+
 st.set_page_config(initial_sidebar_state="collapsed")
+
+def create_connection():
+    mydb = mysql.connect(
+        host="sql6.freemysqlhosting.net",
+        user="sql6630809", #enter username here
+        password="luzGhrQeWj", #enter password here
+        database="sql6630809"
+    )
+    return mydb
+
+def fetch_mail():
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT email FROM projects;")
+    mail_list = [i[0] for i in cursor.fetchall()]
+    connection.close()
+    return list(mail_list)
+
 
 with open(r"style.css") as f:
     st.markdown(f"<script>{f.read()}</script>",unsafe_allow_html=True)
@@ -20,7 +40,10 @@ st.session_state["Email"] = st.text_input("Enter your Email : ")
 
 if st.columns(5)[2].button("Continue ..."):
     if st.session_state.Name and st.session_state.Email and "@" in st.session_state.Email : 
-        switch_page_button.switch_page("Regression")
+        if st.session_state.Email not in fetch_mail(): 
+            switch_page_button.switch_page("Regression")
+        else: 
+            st.error("User has already taken the project")
     else:    
         st.error("NOTE : please enter valid details to get Continue button!")
 
